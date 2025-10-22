8ac438927636e47285180cde404bb0a52fc99ab5
Julio-cod33z <jcjrvn14@gmail.com>
2025-10-22 09:47:02 -0500

    "db.js configuración actualizada"

--- src/config/db.js
+++ src/config/db.js
@@ -3,14 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Conexión en Railway
+  pool = mysql.createPool(process.env.DATABASE_URL);
+} else {
+  // Conexión local (cuando desarrollas en tu máquina)
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+  });
+}
 
 export default pool;
